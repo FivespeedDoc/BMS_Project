c7542a926f95c5175e4a4a441b41ef7f5cd5c295
ZacharyRE <23102145d@connect.polyu.hk>
2024-11-21 03:36:30 +0800

    <fix> added more javadoc in `ModelException`

--- src/model/ModelException.java
+++ src/model/ModelException.java
@@ -2,7 +2,10 @@ package model;
 
 /**
  * <h3>The {@code ModelException} Exception</h3>
+ * All errors in the Model part should throw this exception.
+ * The other parts should catch this exception to get the error information from the Model.
  * @author FrankYang0610
+ * @author ZacharyRE
  */
 public class ModelException extends Exception {
     public ModelException(String message) {
