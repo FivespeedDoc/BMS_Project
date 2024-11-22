2bd0ad169542e4f8e90bfe9d258457889e7ba586
LesPoar <64912056+LesPoar@users.noreply.github.com>
2024-11-22 12:53:09 +0800

    Update ChangeUserInformationWindow.java

--- src/gui/ChangeUserInformationWindow.java
+++ src/gui/ChangeUserInformationWindow.java
@@ -1,40 +1,28 @@
 package gui;
 
-import controller.Controller;
 import gui.components.*;
 import gui.components.TextField;
 import model.ModelException;
 import model.database.Connection;
-import model.entities.Banquet;
-import org.w3c.dom.Text;
+
 import service.managers.AttendeeAccountsManager;
 
 
 
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.sql.Timestamp;
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
-import static javax.swing.JOptionPane.*;
 
 
 public class ChangeUserInformationWindow {
 
     private AttendeeAccountsManager AAM;
     private JFrame window;
-    private JPanel panel;
-    private GridBagConstraints gbc = new GridBagConstraints();
+
     public ChangeUserInformationWindow(String toBeChanged){
         switch (toBeChanged){
             case "Password":
@@ -200,7 +188,7 @@ public class ChangeUserInformationWindow {
         window.setSize(640,360);
         window.setLocationRelativeTo(null);
 
-        panel = new JPanel();
+        JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         panel.setLayout(new BorderLayout());
 
@@ -208,8 +196,4 @@ public class ChangeUserInformationWindow {
         window.setVisible(true);
 
     }
-
-
-
-
 }
